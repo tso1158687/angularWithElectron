def034295e59b4218bb15ef319a331f647700c50
jason <jason@wishingsoft.com>
2019-06-14 16:22:21 +0800

    add component and router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloComponent } from './page/hello/hello.component';
+import { ContactComponent } from './page/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'hello', component: HelloComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '*', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './page/hello/hello.component';
+import { ContactComponent } from './page/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
